eabf71dccb932d45b765a35e9aee4b14891fc957
majdishami <majdi01@yahoo.com>
2025-03-26 10:42:37 -0700

    f

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, loadEnv } from 'vite'; // Remove PluginOption
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
