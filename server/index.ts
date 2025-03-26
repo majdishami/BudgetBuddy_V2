8af519cd54671dd47ca7d5905e2321bf4420eb40
majdishami <majdi01@yahoo.com>
2025-03-25 21:08:29 -0700

    f

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { createServer } from "http";
 import { createServer as createViteServer } from "vite";
-import viteConfig from "../vite.config"; // Ensure the path is correct
+import viteConfig from "../vite.config.ts"; // Ensure the path is correct
 import dotenv from "dotenv";
 import cors from 'cors';
 import pg from 'pg';
